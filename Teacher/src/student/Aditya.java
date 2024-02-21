ce6a43d19371721e9319f0fbba5e7cd69983d7ef
Adi8817 <adidash560@gmail.com>
2024-02-21 23:46:06 +0530

    one commite form aditya dash

--- Teacher/src/student/Aditya.java
+++ Teacher/src/student/Aditya.java
@@ -4,5 +4,6 @@ public class Aditya {
 public static void main(String[]args) {
 	System.out.println("HIIII good moringin....");
 	System.out.println("HIIII Momy");
+	System.out.println("I am a Mechanical engineering student");
 }
 }
